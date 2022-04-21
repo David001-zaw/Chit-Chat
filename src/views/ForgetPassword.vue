ad478e4b90bf57fdbdba776c09553d36fc615d8b
David001-zaw <davidzaw1111@gmail.com>
2022-04-21 22:02:47 +0630

    finished auth frontend

---
+++ src/views/ForgetPassword.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="login">
+        <router-link :to="{ name: 'Home' }" class="text-2xl font-semibold p-5">&larr;</router-link>
+        <div class="container px-5 py-10 flex flex-col justify-center">
+            <h1 class="title-text text-4xl font-thin mb-3">Forgot Password</h1>
+            <p>Enter your email address, we'll send you the instructions on how to change your password.</p>
+
+            <form action="" autocomplete="off">
+                <div class="input-box w100">
+                    <input type="text" required class="input-box__item" name="email">
+                    <label>EMAIL ADDRESS</label>
+                    <span class="focus-border"></span>
+                </div>
+                <button type="submit" class="block py-3 px-5 tracking-wide font-semibold rounded-md bg-red-500 text-white">SEND</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+
+/*===== Contact Form Input Wave =====*/
+// const labels = document.querySelectorAll('.input-box label')
+// labels.forEach(label => {
+//     label.innerHTML = label.innerText
+//         .split('')
+//         .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+//         .join('')
+// })
+</script>
+
+<style scoped>
+.login{
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    isolation: isolate;
+    color: #fff;
+    background-image: url('../assets/imgs/girl-pose.jpg');
+    background-position: center;
+    background-size: cover;
+}
+.login::after{
+    content: '';
+    inset: 0;
+    position: absolute;
+    background: rgba(0, 0, 0, .8);
+    z-index: -1;
+}
+
+.input-box {
+    position: relative;
+    margin: 20px 0;
+    min-width: 300px;
+}
+.input-box.w100 {
+    width: 100%;
+}
+.input-box__item {
+    background-color: transparent;
+    border: 0;
+    border-bottom: 1px #fff solid;
+    display: block;
+    width: 100%;
+    padding: 10px 0;
+    color: #fff;
+    font-size: 1rem;
+    letter-spacing: 1;
+}
+.input-box__item:focus, .input-box__item:valid {
+    outline: 0;
+}
+.input-box__item:focus ~ label span, .input-box__item:valid ~ label span {
+    color: pink;
+    transform: translateY(-20px);
+}
+.input-box__item ~ .focus-border {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    width: 0;
+    height: 1px;
+    background-color: pink;
+    transition: 0.4s;
+}
+.input-box__item:focus ~ .focus-border {
+    width: 100%;
+    transition: 0.4s;
+    left: 0;
+}
+.input-box label {
+    position: absolute;
+    top: 10px;
+    left: 0;
+    pointer-events: none;
+}
+.input-box label span {
+    display: inline-block;
+    font-size: 0.9rem;
+    min-width: 5px;
+    transition: 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container px-5 flex flex-col justify-around">
+      <div class="title">
+        <h1 class="title-text text-3xl tracking-widest mb-3">Chit Chat</h1>
+        <p class="font-thin">Flirt, chat and meet people around you.</p>
+      </div>
+      <div class="buttons w-9/12 mx-auto">
+        <router-link :to="{ name: 'Signup' }" class="block py-3 tracking-wide rounded-md bg-red-500 text-white">SIGN UP NOW</router-link><br>
+        <router-link :to="{ name: 'Login' }" class="block py-3 tracking-wide rounded-md bg-blue-900 text-white">LOGIN</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,3 +24,29 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400&family=Roboto:wght@100;300;400;500&display=swap');
+
+.home{
+  min-height: 100vh;
+  position: relative;
+  isolation: isolate;
+  color: #fff;
+  text-align: center;
+  background-image: url('../assets/imgs/girl-pose.jpg');
+  background-position: center;
+  background-size: cover;
+}
+.home::after{
+  content: '';
+  inset: 0;
+  position: absolute;
+  background: rgba(0, 0, 0, .8);
+  z-index: -1;
+}
+.container{
+  min-height: 100vh;
+}
+
+</style>
